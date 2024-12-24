ed53ad6c224bd634c7ef0cdfad624d1efd5e5911
Lukas Dullinger <129603980+itislu@users.noreply.github.com>
2024-12-24 15:42:53 +0100

    [23] feat: Solve exercise 2
    Solve with Bron-Kerbosch algorithm, but without exclusion set as it seemed redundant without the pivot extension to the algorithm.

--- 2024/day23/src/main.rs
+++ 2024/day23/src/main.rs
@@ -5,6 +5,7 @@ use utils::input;
 fn main() {
     let input = input::read_input();
     println!("exercise 1: {}", exercise1(&input));
+    println!("exercise 2: {}", exercise2(&input));
 }
 
 /*
@@ -13,67 +14,111 @@ for each K:
         is any other V in values of V?
 */
 fn exercise1(input: &str) -> usize {
-    let network = Network::new(input);
+    let network = parse_network(input);
 
-    network
-        .get_threeway_connections()
+    get_threeway_connections(&network)
         .filter(|con| con.iter().any(|node| node.starts_with("t")))
         .count()
 }
 
-struct Network {
-    connections: HashMap<String, HashSet<String>>,
+/*
+Find maximal clique for each node.
+Keep it if it is bigger than the biggest so far.
+*/
+fn exercise2(input: &str) -> String {
+    let network = parse_network(input);
+
+    bron_kerbosch(&network, HashSet::new(), network.keys().copied().collect())
+        .iter()
+        .sorted()
+        .join(",")
 }
 
-impl Network {
-    fn new(input: &str) -> Network {
-        let mut network = Network {
-            connections: HashMap::new(),
-        };
+fn bron_kerbosch<'a>(
+    network: &'a HashMap<&'a str, HashSet<&'a str>>,
+    clique: HashSet<&'a str>,
+    mut candidates: HashSet<&'a str>,
+) -> HashSet<&'a str> {
+    if candidates.is_empty() {
+        return clique;
+    }
 
-        for line in input.lines() {
-            let split: Vec<&str> = line.split('-').collect();
-            network.add_twoway_connection(split[0], split[1]);
-            network.add_twoway_connection(split[1], split[0]);
+    let mut max_clique = clique.clone();
+
+    while let Some(&candidate) = candidates.iter().next() {
+        candidates.remove(candidate);
+
+        let mut new_clique = clique.clone();
+        new_clique.insert(candidate);
+
+        let result = bron_kerbosch(
+            network,
+            new_clique,
+            candidates
+                .intersection(&network[candidate])
+                .copied()
+                .collect(),
+        );
+        if result.len() > max_clique.len() {
+            max_clique = result;
         }
-        network
     }
 
-    fn add_oneway_connection(&mut self, from: &str, to: &str) {
-        if let Some(entry) = self.connections.get_mut(from) {
-            entry.insert(to.to_string());
-        } else {
-            let mut set: HashSet<String> = HashSet::new();
-            set.insert(to.to_string());
-            self.connections.insert(from.to_string(), set);
-        }
-    }
+    max_clique
+}
 
-    fn add_twoway_connection(&mut self, node1: &str, node2: &str) {
-        self.add_oneway_connection(node1, node2);
-        self.add_oneway_connection(node2, node1);
-    }
+fn parse_network(input: &str) -> HashMap<&str, HashSet<&str>> {
+    let mut network = HashMap::new();
 
-    fn get_threeway_connections(&self) -> impl Iterator<Item = Vec<&String>> {
-        self.connections
-            .keys()
-            .flat_map(|key| {
-                self.connections[key]
-                    .iter()
-                    .combinations(2)
-                    .filter_map(move |pair| {
-                        if self.connections[pair[0]].contains(pair[1]) {
-                            let mut three_way_connection = vec![key, pair[0], pair[1]];
-                            three_way_connection.sort();
-                            Some(three_way_connection)
-                        } else {
-                            None
-                        }
-                    })
+    for line in input.lines() {
+        let split: Vec<&str> = line.split('-').collect();
+        add_twoway_connection(&mut network, split[0], split[1]);
+        add_twoway_connection(&mut network, split[1], split[0]);
+    }
+    network
+}
+
+fn add_oneway_connection<'a>(
+    network: &mut HashMap<&'a str, HashSet<&'a str>>,
+    from: &'a str,
+    to: &'a str,
+) {
+    if let Some(entry) = network.get_mut(from) {
+        entry.insert(to);
+    } else {
+        let mut set: HashSet<&str> = HashSet::new();
+        set.insert(to);
+        network.insert(from, set);
+    }
+}
+
+fn add_twoway_connection<'a>(
+    network: &mut HashMap<&'a str, HashSet<&'a str>>,
+    node1: &'a str,
+    node2: &'a str,
+) {
+    add_oneway_connection(network, node1, node2);
+    add_oneway_connection(network, node2, node1);
+}
+
+fn get_threeway_connections<'a>(
+    network: &HashMap<&'a str, HashSet<&'a str>>,
+) -> impl Iterator<Item = Vec<&'a str>> {
+    network
+        .keys()
+        .flat_map(|&key| {
+            network[key].iter().combinations(2).filter_map(move |pair| {
+                if network[pair[0]].contains(pair[1]) {
+                    let mut three_way_connection = vec![key, pair[0], pair[1]];
+                    three_way_connection.sort();
+                    Some(three_way_connection)
+                } else {
+                    None
+                }
             })
-            .collect::<HashSet<_>>()
-            .into_iter()
-    }
+        })
+        .collect::<HashSet<_>>()
+        .into_iter()
 }
 
 #[cfg(test)]
@@ -85,5 +130,12 @@ mod test {
         let input = input::read_example();
         let res = exercise1(&input);
         assert_eq!(res, 7);
+    }
+
+    #[test]
+    fn test_ex2() {
+        let input = input::read_example();
+        let res = exercise2(&input);
+        assert_eq!(res, "co,de,ka,ta");
     }
 }
